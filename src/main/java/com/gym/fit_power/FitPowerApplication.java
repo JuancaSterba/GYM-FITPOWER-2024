ec570141e23c3251bc06c777ffd36e9174ec3935
Melinagamarra-2023 <gamarramelina.2002@gmail.com>
2024-10-17 09:34:02 -0300

    first commit

---
+++ src/main/java/com/gym/fit_power/FitPowerApplication.java
@@ -0,0 +1,13 @@
+package com.gym.fit_power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitPowerApplication.class, args);
+	}
+
+}
