4e5018667d43607b851ccb8cb0a989427a782cdc
KevinTurraca <kevin.itec1@gmail.com>
2024-10-29 16:03:38 -0300

    creadas las constantes
    también se actualizó el dto

--- src/main/java/com/gym/fit_power/dto/ClientDTO.java
+++ src/main/java/com/gym/fit_power/dto/ClientDTO.java
@@ -3,6 +3,7 @@ package com.gym.fit_power.dto;
 import lombok.Data;
 import java.util.List;
 import lombok.NoArgsConstructor;
+import com.gym.fit_power.model.Gym;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
@@ -13,7 +14,7 @@ public class ClientDTO {
     private Long id;
 
     private String cuit;
-    private String assignedGym;
+    private Gym assignedGym;
     private String name;
     private String lastname;
     private String email;
@@ -25,5 +26,8 @@ public class ClientDTO {
     private String corpulence;
     private List<String> allergies;
     private String birthDate;
+    private String createdAt;
+    private String updatedAt;
+    private Boolean enabled;
 
 }
