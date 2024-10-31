eaadd9f836358149e940fe30bc8076607255ec11
Melinagamarra-2023 <gamarramelina.2002@gmail.com>
2024-10-31 10:24:09 -0300

    Entidades
    Se actualizó nutricionista, se creó la entidades de plan de nutrición y diaria de nutrición.

--- src/main/java/com/gym/fit_power/model/NutritionPlan.java
+++ src/main/java/com/gym/fit_power/model/NutritionPlan.java
@@ -1,6 +1,8 @@
 package com.gym.fit_power.model;
 
 import lombok.Data;
+
+import java.time.LocalDate;
 import java.util.List;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,20 @@ public class NutritionPlan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private LocalDate creatAt;
+    private Float dailyCalories;
+    private Float dailyCarbohydrates;
+    private Float dailyProteins;
+    private Float dailyFats;
+    private Float desiredWeight;
+    private Boolean enabled;
+    private Nutritionist nutritionist;
 
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
     @OneToMany(mappedBy = "nutrition_plan")
-    private List<NutritionPlan> plans;
+    private List<NutritionDiary> logNutri;
+
 
 }
