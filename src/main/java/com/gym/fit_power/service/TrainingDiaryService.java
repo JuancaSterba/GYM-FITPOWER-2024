58970683de3a97e9cbc52dbc382b7a5254f411cb
JuancaSterba <sjcexe@gmail.com>
2024-11-19 15:49:50 -0300

    Se crea metodo save para TrainingDiary
    Junto a ello se crean los dto y  el mapper, se modifican los servicios para que contengan los metodos necesarios

--- src/main/java/com/gym/fit_power/service/TrainingDiaryService.java
+++ src/main/java/com/gym/fit_power/service/TrainingDiaryService.java
@@ -1,4 +1,8 @@
 package com.gym.fit_power.service;
 
+import com.gym.fit_power.dto.request.TrainingDiaryRequestDto;
+import com.gym.fit_power.dto.response.TrainingDiaryResponseDto;
+
 public interface TrainingDiaryService {
+    TrainingDiaryResponseDto save(TrainingDiaryRequestDto trainingDiaryRequestDto);
 }
--- src/main/java/com/gym/fit_power/service/impl/TrainingDiaryServiceImpl.java
+++ src/main/java/com/gym/fit_power/service/impl/TrainingDiaryServiceImpl.java
@@ -1,17 +1,37 @@
 package com.gym.fit_power.service.impl;
 
+import com.gym.fit_power.dto.request.TrainingDiaryRequestDto;
+import com.gym.fit_power.dto.response.TrainingDiaryResponseDto;
+import com.gym.fit_power.mapper.TrainingDiaryMapper;
+import com.gym.fit_power.model.Routine;
+import com.gym.fit_power.model.TrainingDiary;
+import com.gym.fit_power.repository.RoutineRepository;
 import com.gym.fit_power.repository.TrainingDiaryRepository;
 import com.gym.fit_power.service.TrainingDiaryService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TrainingDiaryServiceImpl implements TrainingDiaryService {
 
     private final TrainingDiaryRepository trainingDiaryRepository;
+    private final RoutineRepository routineRepository;
 
-    public TrainingDiaryServiceImpl(TrainingDiaryRepository trainingDiaryRepository) {
+    public TrainingDiaryServiceImpl(TrainingDiaryRepository trainingDiaryRepository, RoutineRepository routineRepository) {
         this.trainingDiaryRepository = trainingDiaryRepository;
+        this.routineRepository = routineRepository;
     }
 
-    
+
+    @Override
+    public TrainingDiaryResponseDto save(TrainingDiaryRequestDto trainingDiaryRequestDto) {
+        Optional<Routine> routine = routineRepository.findById(trainingDiaryRequestDto.getRoutineId());
+        if (routine.isEmpty()) {
+            throw new RuntimeException("Routine not found");
+        }
+        TrainingDiary trainingDiary = TrainingDiaryMapper.toEntity(trainingDiaryRequestDto);
+        trainingDiary.setRoutine(routine.get()); // Asigna el ID de la rutina al objeto TrainingDiary
+        return TrainingDiaryMapper.toDto(trainingDiaryRepository.save(trainingDiary));
+    }
 }
