a130206784b533715fed1e3bb59a38ab480ba716
KevinTurraca <kevin.itec1@gmail.com>
2024-11-23 18:48:47 -0300

    servicio de envio de emails agregado

--- src/main/java/com/gym/fit_power/notification/ClientEventsNotification.java
+++ src/main/java/com/gym/fit_power/notification/ClientEventsNotification.java
@@ -10,17 +10,23 @@ import org.springframework.kafka.annotation.KafkaListener;
 @Component
 public class ClientEventsNotification{
 
-    @KafkaListener(
-            topics = "${topic.client.name:clients}",
-            containerFactory = "kafkaListenerContainerFactory",
-            groupId = "group1")
+    private final MailSenderService mailSender;
+
+    public ClientEventsNotification(MailSenderService mailSender){
+        this.mailSender = mailSender;
+    }
+
+    @KafkaListener(topics = "${topic.client.name:clients}",
+            containerFactory = "kafkaListenerContainerFactory", groupId = "group1")
     public void consumer(Event<?> event) {
         if (event.getClass().isAssignableFrom(ClientCreatedEvent.class)) {
             ClientCreatedEvent createdEvent = (ClientCreatedEvent) event;
             log.info("Received created event for client with Id={}, data={}",
-                    createdEvent.getId(),
-                    createdEvent.getData().toString());
-
+                    createdEvent.getId(), createdEvent.getData().toString());
+            mailSender.send(createdEvent.getData().getEmail(),
+                    "Bienvenido a FitPower!!",
+                    "Se ha recibido la solicitud de creación para un nuevo cliente de la aplicación FitPower\n" +
+                            "si desconoce esta solicitud por favor comuníquese con el servicio técnico.");
         }
     }
 }
