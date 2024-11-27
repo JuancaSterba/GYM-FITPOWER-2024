088e6e30d33dd1e09e1aa9143caa0f743ef44ca6
Melinagamarra-2023 <gamarramelina.2002@gmail.com>
2024-11-27 09:06:28 -0300

    serviceNutri

--- src/main/java/com/gym/fit_power/repository/NutritionPlanRepository.java
+++ src/main/java/com/gym/fit_power/repository/NutritionPlanRepository.java
@@ -1,5 +1,6 @@
 package com.gym.fit_power.repository;
 
+
 import com.gym.fit_power.model.NutritionPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
