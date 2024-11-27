2c3e403475a8d0b8ecd82385d2a98f2329da5956
JuancaSterba <sjcexe@gmail.com>
2024-11-27 16:26:46 -0300

    Update data.sql
    Se agregan rutinas con sus respectivos ejercicios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,22 @@ INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejerci
 
 INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 3', 'Descripción del ejercicio 3', 'Grupo muscular 2', TRUE);
 
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 4', 'Descripción del ejercicio 4', 'Grupo muscular 2', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 5', 'Descripción del ejercicio 5', 'Grupo muscular 3', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 6', 'Descripción del ejercicio 6', 'Grupo muscular 3', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 7', 'Descripción del ejercicio 7', 'Grupo muscular 4', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 8', 'Descripción del ejercicio 8', 'Grupo muscular 4', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 9', 'Descripción del ejercicio 9', 'Grupo muscular 5', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 10', 'Descripción del ejercicio 10', 'Grupo muscular 5', TRUE);
+
+INSERT INTO exercises (name, description, muscle_group, enabled) VALUES ('Ejercicio 11', 'Descripción del ejercicio 11', 'Grupo muscular 6', TRUE);
+
 INSERT INTO gyms (address, domain, mail, phone, enabled) VALUES ('Calle 123', 'gimnasio.com', 'gimnasio@example', '1234567890', TRUE);
 
 INSERT INTO gyms (address, domain, mail, phone, enabled) VALUES ('Calle 456', 'gimnasio2.com', 'gimnasio2@example', '9876543210', TRUE);
@@ -39,3 +55,28 @@ INSERT INTO nutritionists (cuit, name, lastname, email, phone, enabled, created_
 INSERT INTO nutritionists (cuit, name, lastname, email, phone, enabled, created_at) VALUES ('33456789015', 'Nutricionista 4', 'Díaz', 'nutricionista4@example', '6666666666', TRUE, '2022-04-01');
 
 INSERT INTO nutritionists (cuit, name, lastname, email, phone, enabled, created_at) VALUES ('20567890125', 'Nutricionista 5', 'Martínez', 'nutricionista5@example', '7777777777', TRUE, '2022-05-01');
+
+INSERT INTO routines (goals, created_at, trainer_id, client_id, active) VALUES ('Objetivo 1', '2022-01-01', 1, 1, TRUE);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (10, 3, 30, 1, 1);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (12, 4, 45, 1, 2);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (8, 2, 60, 1, 3);
+
+INSERT INTO routines (goals, created_at, trainer_id, client_id, active) VALUES ('Objetivo 2', '2022-02-01', 2, 2, TRUE);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (15, 5, 30, 2, 1);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (10, 3, 45, 2, 4);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (12, 4, 60, 2, 5);
+
+INSERT INTO routines (goals, created_at, trainer_id, client_id, active) VALUES ('Objetivo 3', '2022-03-01', 3, 3, TRUE);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (8, 2, 30, 3, 2);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (12, 4, 45, 3, 6);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (10, 3, 60, 3, 7);
+
+INSERT INTO routines (goals, created_at, trainer_id, client_id, active) VALUES ('Objetivo 4', '2022-04-01', 4, 4, TRUE);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (12, 4, 30, 4, 3);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (15, 5, 45, 4, 8);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (10, 3, 60, 4, 9);
+
+INSERT INTO routines (goals, created_at, trainer_id, client_id, active) VALUES ('Objetivo 5', '2022-05-01', 5, 5, TRUE);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (10, 3, 30, 5, 4);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (12, 4, 45, 5, 10);
+INSERT INTO exercise_sets (reps, sets, rest_in_minutes, routine_id, exercise_id) VALUES (8, 2, 60, 5, 11);
