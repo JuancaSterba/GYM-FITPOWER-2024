9c996160f5b3cb1c9ecebba307e6f35847b15a4f
Melinagamarra-2023 <gamarramelina.2002@gmail.com>
2024-12-03 14:25:37 -0300

    se agrego la seguridad al codigo

---
+++ src/main/java/com/gym/fit_power/security/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.gym.fit_power.security.controller;
+
+import com.gym.fit_power.security.dto.AuthResponse;
+import com.gym.fit_power.security.dto.LoginRequest;
+import com.gym.fit_power.security.dto.RegisterRequest;
+import com.gym.fit_power.security.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+
+}
---
+++ src/main/java/com/gym/fit_power/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.gym.fit_power.security.repository;
+
+
+import com.gym.fit_power.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+}
